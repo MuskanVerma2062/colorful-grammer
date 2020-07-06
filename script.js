e1290d2bcfa367f264f135c800518cd746ac73fe
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-07-06 15:13:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+_const wordpos = new WordPOS({
+  dictPath: "https://cdn.jsdelivr.net/npm/wordpos-web@1.0.2/dict"
+})
+
+$(document).ready(() => {
+  updateBackground = () => {
+    let sentence = $("#sentence").val()
+    wordpos.getPOS(sentence).then(types => {
+      const leftGradient = (((types.verbs.length +
+        types.adverbs.length +
+        types.nouns.length +
+        types.adjectives.length) * 4) + (types.rest.length / 20))
+
+      const rightGradient = sentence.length
+
+      $("body").css("background-image", `linear-gradient(to right,
+        hsl(${leftGradient}, 100%, 60%), hsl(${rightGradient}, 100%, 60%)
+      `)
+    })
+  }
+  updateBackground()
+  $("#sentence").on("keydown", updateBackground)
+})
